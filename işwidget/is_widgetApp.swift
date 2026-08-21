560832b4f910089ce74ef85b965e6b899d3c3c9b
pixlordrd <297255815+pixlordrd@users.noreply.github.com>
2026-08-21 16:20:20 +0300

    Initial Commit

---
+++ işwidget/is_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  is_widgetApp.swift
+//  işwidget
+//
+//  Created by Erdem ÖZDEN on 21.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct is_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ işwidgetTests/is_widgetTests.swift
@@ -0,0 +1,19 @@
+//
+//  is_widgetTests.swift
+//  işwidgetTests
+//
+//  Created by Erdem ÖZDEN on 21.08.2026.
+//
+
+import Testing
+@testable import is_widget
+
+struct is_widgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
